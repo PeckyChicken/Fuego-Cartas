34f12cc87c351b891c65984de7f0a64f5cc2cf3a
Benjy <BenjyPeckChicken@gmail.com>
2024-11-30 09:51:23 +0000

    FEATURE: Data names are now case-insensitive. FEATURE: Spaces before and after the = are stripped. FEATURE: Data is now run through json loading, so it is correctly typed.

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ data = {}
 with open("config.cfg") as f:
     for x in f.readlines():
         datum = x.split("=")
-        data[datum[0]] = datum[1]
-
+        data[datum[0].strip()] = datum[1].strip()
 
 def get(name,fallback=None):
-    if name in data:
-        return data[name]
+    if name.upper() in data:
+        return json.loads(data[name.upper()])
     return fallback
