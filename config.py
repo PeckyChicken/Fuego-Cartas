9b9360d81287f053966af5f42bac34c544105854
Benjy <BenjyPeckChicken@gmail.com>
2024-12-14 15:30:16 +0000

    QOL: Proper namespacing

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 
 data = {}
 with open("config.cfg") as f:
@@ -6,7 +7,7 @@ with open("config.cfg") as f:
         datum = x.split("=")
         data[datum[0].strip()] = datum[1].strip()
 
-def get(name,fallback=None):
+def get(name,fallback=None) -> Any:
     if name.upper() in data:
         return json.loads(data[name.upper()])
     return fallback
