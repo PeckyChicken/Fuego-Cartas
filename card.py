822d36d9271428e9840f30de7766ca5e6ecbb5f4
Benjy <BenjyPeckChicken@gmail.com>
2024-12-14 10:21:53 +0000

    TEMP

--- card.py
+++ card.py
@@ -4,5 +4,12 @@ from PIL import Image,ImageTk
 
 class Card:
     def __init__(self,color:int,value:int):
-        img = ImageTk.PhotoImage(colors.shift(gui.card_tileset.get(0,1),78))
-        gui.c.create_image(300,300,image=img)
+        tileset_coords = gui.card_tileset.index_to_coords(value)
+        print(gui.card_tileset.get(*tileset_coords))
+        self.img = ImageTk.PhotoImage(colors.shift(gui.card_tileset.get(*tileset_coords),color))
+        gui.c.create_rectangle(300,300,502,603,width=5)
+        self.id = gui.c.create_image(300,300,image=self.img)
+        print(tileset_coords,self.img,self.id)
+
+img = ImageTk.PhotoImage(colors.shift(gui.card_tileset.get(0,0),48))
+id1 = gui.c.create_image(200,200,image=img)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gui
 import card
 
+card.Card(78,11)
 
 gui.window.mainloop()
