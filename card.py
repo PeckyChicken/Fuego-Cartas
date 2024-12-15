ed61d2b88c43a799037759183ee17b0b5fc66e10
Benjy <BenjyPeckChicken@gmail.com>
2024-12-15 06:59:08 +0000

    FEATURE: Hand space rectangle now drawn

--- card.py
+++ card.py
@@ -1,11 +1,12 @@
+from PIL import Image, ImageTk
+
+import config
 import gui
 import imaging
-from PIL import Image,ImageTk
+
 
 class Card:
     def __init__(self,color:int,value:int):
-        gui.c.create_rectangle(300,300,502,603,width=5)
-
         self.tileset_coords = gui.card_tileset.index_to_coords(value)
         self.color = color
 
@@ -23,3 +24,18 @@ class Card:
     def scale(self,scale):
         self.image = imaging.scale(self.image,scale)
         self.redraw()
+
+def draw_hand_space():
+    coords = config.get("hand_coords")
+    width = config.get("hand_max_width")
+    height = config.get("card_height")
+    scale = config.get("hand_card_scale")
+
+    x1 = coords[0]-width/2
+    y1 = coords[1]-(height/2)*scale
+    x2 = coords[0]+width/2
+    y2 = coords[1]+(height/2)*scale
+
+    gui.c.create_rectangle(x1,y1,x2,y2,width=5)
+
+draw_hand_space()
