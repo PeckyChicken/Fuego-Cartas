145276f407437ea81ab1065891309b066cb2caf5
Benjy <BenjyPeckChicken@gmail.com>
2024-12-15 08:10:23 +0000

    Added hand for testing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import card
+import config
 import gui
+import hand
 
-a = card.Card(0,11)
+player_hand = hand.Hand((config.get("window_width")/2,config.get("hand_y")),...)
+a = card.Card(180,11)
 a.scale(0.5)
+a.move_to(*player_hand.coords)
 
 gui.window.mainloop()
